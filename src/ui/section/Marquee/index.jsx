69e0e77d3e91a0edd47946a84f74cbe0881147de
Shailly-Sahay <shaillysahayy@gmail.com>
2025-03-07 17:20:43 +0530

    marquee overflow

--- src/ui/section/Marquee/index.jsx
+++ src/ui/section/Marquee/index.jsx
@@ -3,7 +3,7 @@ import { marquee } from "../../../data/data";
 
 const Marquee = () => {
   return (
-    <section className="flex gap-24 justify-center !mb-32">
+    <section className="flex gap-24 justify-center !mb-32 overflow-x-hidden">
       {marquee.map((el) => (
         <div className="flex items-center">
           <p className="outlined-text font-semibold uppercase relative before:absolute before:bg-[url('/assets/svg/star.svg')] before:w-[32px] before:h-[32px] before:top-[50%] before:left-[102%] before:translate-x-0 before:-translate-y-1/2 before:bg-no-repeat">
